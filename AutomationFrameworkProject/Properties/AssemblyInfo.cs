d0587b2fefae76f3c1044d53dbb9f30188984cad
RaahimSiddiqi <k200146@nu.edu.pk>
2024-05-05 22:27:09 +0500

    inital commit

---
+++ AutomationFrameworkProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutomationFrameworkProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("AutomationFrameworkProject")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("00ba73bc-37b0-4773-b0c5-52a2ba831a3d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AutomationFrameworkProject/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace AutomationFrameworkProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver driver = new ChromeDriver();
+
+        [TestMethod]
+        public void LoginWith_ValidUser_ValidPassword_TC001()
+        {
+            Login("https://www.saucedemo.com/", "standard_user", "secret_sauce");
+            string title = driver.FindElement(By.XPath("//div[@class='header_secondary_container']/span[@class='title']")).Text;
+            Assert.AreEqual(title, "Products");
+            driver.Close();
+        }
+
+        [TestMethod]
+        public void LoginWith_InvalidUser_ValidPassword_TC002()
+        {
+            Login("https://www.saucedemo.com/", "incorrect_user", "secret_sauce");
+            string error = driver.FindElement(By.XPath("//div[contains(@class, 'error-message-container')]/h3")).Text;
+            Assert.AreEqual(error, "Epic sadface: Username and password do not match any user in this service");
+            driver.Close();
+        }
+
+        [TestMethod]
+        public void LoginWith_InvalidUser_InvalidPassword_TC003()
+        {
+            Login("https://www.saucedemo.com/", "incorrect_user", "incorrect_sauce");
+            string error = driver.FindElement(By.XPath("//div[contains(@class, 'error-message-container')]/h3")).Text;
+            Assert.AreEqual(error, "Epic sadface: Username and password do not match any user in this service");
+            driver.Close();
+        }
+
+        public void Login(string url, string username, string password)
+        {
+            driver.Url = url;
+            driver.FindElement(By.Id("user-name")).SendKeys(username);
+            driver.FindElement(By.Id("password")).SendKeys(password);
+            driver.FindElement(By.Id("login-button")).Click();
+        }
+    }
+}
